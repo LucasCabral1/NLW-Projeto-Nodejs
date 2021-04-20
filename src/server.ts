f83a838eddbdc73e36ce609f3372600ea072f1ea
Lucas Cabral <lucabral.contato@gmail.com>
2021-04-20 16:27:18 -0300

    Primeiro commit

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express' // ... -> significa que precisa ser instalado a tipagem do express
+const app = express() // executar o express
+
+/*
+Método HTPP
+GET - buscar informações
+POST - Criar 
+PUT - Alteração
+DELETE - Excluir
+PATCH - Alterar uma informação específica
+*/
+
+app.get("/", (request, response) => {// '/' - rota da URL, req - tudo que o usuario envia, res - a resposta do servidor
+    return response.json({
+        "message": "Olá NLW 05"
+    })
+})
+
+app.post("/users", (req, res)=>{
+    return res.json({ message: "Usuário salvo com sucesso"})
+})
+
+
+app.listen(3333, ()=>{
+    console.log('Server is running on port 3333') // criação do servidor
+})
